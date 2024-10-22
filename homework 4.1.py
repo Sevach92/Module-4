fc1e38d3f3d1f2420757cee3a74f84f49241c58b
Vsevolod <sevach92@yandex.ru>
2024-10-22 14:16:43 +0400

    Add files via upload

---
+++ homework 4.1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as Целое
+from true_math import  divide as Бесконечность
+result1 =  Целое(69, 3)
+result2 =  Целое(3, 0)
+result3 = Бесконечность(49, 7)
+result4 = Бесконечность(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
